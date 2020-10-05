11ad02ec81e8a7983a235794b4b42d9c1193dcfc
Michelle Ranly <meranly8@gmail.com>
2020-10-05 23:26:46 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -12,17 +12,10 @@ class Song
 	end
 
 	def self.new_by_filename(filename)
-	 	file_split = filename.split(" - ")
-		name = file_split[0]
-		artist = file_split[1]
-		song = self.new(name)
-		song.artist = artist_name(artist)
+	  artist, song_name = filename.split(" - ")
+		song = self.new(song_name)
+		song.artist_name = artist
 		song
-	 
-	 # artist, song_name = filename.split(" - ")
-		# song = self.new(song_name)
-		# song.artist_name = artist
-		# song
 	end
 
 	def artist_name=(name)
